ad649a2d585a94173badce8fbda81ea0b3fcbbfe
ArpitMarkana <127651326+ArpitMarkana@users.noreply.github.com>
2023-03-12 21:42:16 +0530

    Delete valid.js

--- valid.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('Sign-Up').addEventListener('submit',(event)=>{
-    event.preventDefault();
-});
-document.getElementById('Sign-In').addEventListener('submit',(event)=>{
-    event.preventDefault();
-});
