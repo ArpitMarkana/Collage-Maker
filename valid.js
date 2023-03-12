66b630bc03ed7fddd9fdb1191a4fb8561d8f367d
ArpitMarkana <127651326+ArpitMarkana@users.noreply.github.com>
2023-03-12 18:20:59 +0530

    Update valid.js

---
+++ valid.js
@@ -0,0 +1,6 @@
+document.getElementById('Sign-Up').addEventListener('submit',(event)=>{
+    event.preventDefault();
+});
+document.getElementById('Sign-In').addEventListener('submit',(event)=>{
+    event.preventDefault();
+});
